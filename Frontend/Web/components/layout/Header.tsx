24f14974d471a1688038fdf0e0d510551041af23
abransh <abranshbaliyan2807@gmail.com>
2025-03-01 18:02:06 +0100

    header update

--- Frontend/Web/components/layout/Header.tsx
+++ Frontend/Web/components/layout/Header.tsx
@@ -3,8 +3,26 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const Header: React.FC = () => {
-  const
-}; 
+  const [visible, setVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [scrollSpeed, setScrollSpeed] = useState(0);
+  const [lastScrollTime, setLastScrollTime] = useState(Date.now());
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const currentTime = Date.now();
+      const timeDiff = currentTime - lastScrollTime;
+      
+      // Calculate scroll speed (pixels per millisecond)
+      const speed = Math.abs(currentScrollPos - prevScrollPos) / (timeDiff || 1);
+      setScrollSpeed(speed);
+      
+      // Update time of last scroll
+      setLastScrollTime(currentTime);
+      
+
+
 
 return(
   <div>
--- Frontend/Web/components/layout/dist/header.js
+++ Frontend/Web/components/layout/dist/header.js
@@ -1,7 +1,23 @@
 "use strict";
 exports.__esModule = true;
+var react_1 = require("react");
 var Header = function () {
-    var ;
+    var _a = react_1.useState(true), visible = _a[0], setVisible = _a[1];
+    var _b = react_1.useState(0), prevScrollPos = _b[0], setPrevScrollPos = _b[1];
+    var _c = react_1.useState(0), scrollSpeed = _c[0], setScrollSpeed = _c[1];
+    var _d = react_1.useState(Date.now()), lastScrollTime = _d[0], setLastScrollTime = _d[1];
+    react_1.useEffect(function () {
+        var handleScroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            var currentTime = Date.now();
+            var timeDiff = currentTime - lastScrollTime;
+            // Calculate scroll speed (pixels per millisecond)
+            var speed = Math.abs(currentScrollPos - prevScrollPos) / (timeDiff || 1);
+            setScrollSpeed(speed);
+            // Update time of last scroll
+            setLastScrollTime(currentTime);
+            return (react_1["default"].createElement("div", null));
+            export default Header;
+        };
+    });
 };
-return (react_1["default"].createElement("div", null));
-exports["default"] = Header;
