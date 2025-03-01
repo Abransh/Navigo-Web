e331bf257f5a84acaced050355b9c8a795f110be
abransh <abranshbaliyan2807@gmail.com>
2025-03-01 18:01:27 +0100

    header update

--- Frontend/Web/components/layout/dist/header.js
+++ Frontend/Web/components/layout/dist/header.js
@@ -1,22 +1,7 @@
 "use strict";
 exports.__esModule = true;
-// frontend/web/components/layout/Header.tsx
-var link_1 = require("next/link");
-var react_1 = require("react");
 var Header = function () {
-    return (react_1["default"].createElement("div", { className: "flex justify-center px-4" },
-        react_1["default"].createElement("header", { className: "flex justify-between items-center" },
-            react_1["default"].createElement("div", { className: "w-[373px] h-[32px] bg-white/80 backdrop-blur-sm rounded-full shadow-md flex items-center px-6" },
-                react_1["default"].createElement(link_1["default"], { href: "/", className: "text-xl font text-[#000000]" }, "Navigo")),
-            react_1["default"].createElement("div", { className: "w-[622px] h-[32px] bg-white/80 backdrop-blur-sm rounded-full shadow-md mx-2 flex justify-center items-center" },
-                react_1["default"].createElement("nav", { className: "flex space-x-6" },
-                    react_1["default"].createElement(link_1["default"], { href: "/explore", className: "hover:text-[#F3A522] text-sm" }, "Explore Destinations"),
-                    react_1["default"].createElement(link_1["default"], { href: "/plan", className: "hover:text-[#F3A522] text-sm" }, "Plan Your Trip"),
-                    react_1["default"].createElement(link_1["default"], { href: "/magazine", className: "hover:text-[#F3A522] text-sm" }, "Magazine"),
-                    react_1["default"].createElement(link_1["default"], { href: "/about", className: "hover:text-[#F3A522] text-sm" }, "About us"))),
-            react_1["default"].createElement("div", { className: "w-[249px] h-[32px] bg-white/80 backdrop-blur-sm rounded-full shadow-md flex items-center px-6 justify-between group hover:bg-[#6babea] transition-all duration-300" },
-                react_1["default"].createElement(link_1["default"], { href: "/trip-planner", className: "text-sm group-hover:text-white transition-colors duration-300" }, "Plan Your Trip"),
-                react_1["default"].createElement("svg", { className: "w-4 h-4 transform group-hover:rotate-45 transition-transform duration-300", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" },
-                    react_1["default"].createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, d: "M14 5l7 7m0 0l-7 7m7-7H3" }))))));
+    var ;
 };
+return (react_1["default"].createElement("div", null));
 exports["default"] = Header;
