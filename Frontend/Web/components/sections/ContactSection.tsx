4f4541a7465b9457b5ce2e896e41eb1956c0fbab
abransh <abranshbaliyan2807@gmail.com>
2025-03-06 02:56:23 +0100

    making contact us

--- Frontend/Web/components/sections/ContactSection.tsx
+++ Frontend/Web/components/sections/ContactSection.tsx
@@ -1,38 +1,38 @@
-"use client"; 
+// "use client"; 
 
-import type React from "react";
+// import type React from "react";
 
-import { useState } from "react";
-import Image from "next/image";
-import { Calendar } from "lucide-react";
-import { format } from "date-fns";
-import { Calendar } from "@/components/ui/calendar";
-import { Popover,  PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { cn } from "@/lib/utils"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+// import { useState } from "react";
+// import Image from "next/image";
+// import { Calendar } from "lucide-react";
+// import { format } from "date-fns";
+// import { Calendar } from "@/components/ui/calendar";
+// import { Popover,  PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+// import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
+// import { cn } from "@/lib/utils"
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 
-const destinations = [
-    { value: "varanasi", label: "Varanasi" },
-    { value: "goa", label: "Goa" },
-    { value: "new-delhi", label: "New Delhi" },
-    { value: "jaipur", label: "Jaipur" },
-    { value: "mumbai", label: "Mumbai" },
-    { value: "agra", label: "Agra" },
-    { value: "udaipur", label: "Udaipur" },
-  ]
+// const destinations = [
+//     { value: "varanasi", label: "Varanasi" },
+//     { value: "goa", label: "Goa" },
+//     { value: "new-delhi", label: "New Delhi" },
+//     { value: "jaipur", label: "Jaipur" },
+//     { value: "mumbai", label: "Mumbai" },
+//     { value: "agra", label: "Agra" },
+//     { value: "udaipur", label: "Udaipur" },
+//   ]
 
-  export default function ContactSection () {
-    const[date, setDate] = useState<Date | undefined>(undefined)
-    const [dateRange, setDateRange] = useState<{
-        from: Date | undefined
-        to: Date | undefined
-      }>({
-        from: undefined,
-        to: undefined,
-      })
-//continue here making contact form 
-  }
+//   export default function ContactSection () {
+//     const[date, setDate] = useState<Date | undefined>(undefined)
+//     const [dateRange, setDateRange] = useState<{
+//         from: Date | undefined
+//         to: Date | undefined
+//       }>({
+//         from: undefined,
+//         to: undefined,
+//       })
+// //continue here making contact form 
+//   }
--- Frontend/Web/components/sections/dist/ContactSection.js
+++ Frontend/Web/components/sections/dist/ContactSection.js
@@ -1,22 +1,33 @@
-"use client";
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var destinations = [
-    { value: "varanasi", label: "Varanasi" },
-    { value: "goa", label: "Goa" },
-    { value: "new-delhi", label: "New Delhi" },
-    { value: "jaipur", label: "Jaipur" },
-    { value: "mumbai", label: "Mumbai" },
-    { value: "agra", label: "Agra" },
-    { value: "udaipur", label: "Udaipur" },
-];
-function ContactSection() {
-    var _a = react_1.useState(undefined), date = _a[0], setDate = _a[1];
-    var _b = react_1.useState({
-        from: undefined,
-        to: undefined
-    }), dateRange = _b[0], setDateRange = _b[1];
-    //continue here making contact form 
-}
-exports["default"] = ContactSection;
+// "use client"; 
+// import type React from "react";
+// import { useState } from "react";
+// import Image from "next/image";
+// import { Calendar } from "lucide-react";
+// import { format } from "date-fns";
+// import { Calendar } from "@/components/ui/calendar";
+// import { Popover,  PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+// import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
+// import { cn } from "@/lib/utils"
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+// const destinations = [
+//     { value: "varanasi", label: "Varanasi" },
+//     { value: "goa", label: "Goa" },
+//     { value: "new-delhi", label: "New Delhi" },
+//     { value: "jaipur", label: "Jaipur" },
+//     { value: "mumbai", label: "Mumbai" },
+//     { value: "agra", label: "Agra" },
+//     { value: "udaipur", label: "Udaipur" },
+//   ]
+//   export default function ContactSection () {
+//     const[date, setDate] = useState<Date | undefined>(undefined)
+//     const [dateRange, setDateRange] = useState<{
+//         from: Date | undefined
+//         to: Date | undefined
+//       }>({
+//         from: undefined,
+//         to: undefined,
+//       })
+// //continue here making contact form 
+//   }
